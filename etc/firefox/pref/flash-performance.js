5eaa9a7eadb1b829265ddcb03e8d4b447a06aaf2
Matt Sealey <matt@genesi-usa.com>
2011-02-11 23:03:20 -0600

    add some new fancy toys

---
+++ etc/firefox/pref/flash-performance.js
@@ -0,0 +1,6 @@
+pref("network.http.max-connections", 96);
+pref("network.http.max-connections-per-server", 32);
+pref("network.http.pipelining", true);
+pref("network.http.pipelining.maxrequests", 8);
+pref("network.http.proxy.pipelining", true);
+
---
+++ etc/firefox/pref/genesi-homepage.js
@@ -0,0 +1,1 @@
+pref("browser.startup.homepage", "http://www.genesiamericas.com/");
