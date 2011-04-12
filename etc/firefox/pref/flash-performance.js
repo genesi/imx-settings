6baf4dab3b358d97faa3155150b90ad7895d7f8a
Matt Sealey <matt@genesi-usa.com>
2011-04-11 20:13:03 -0500

    firefox prefs: disable threaded IPC to flash plugin

--- etc/firefox/pref/flash-performance.js
+++ etc/firefox/pref/flash-performance.js
@@ -3,4 +3,5 @@ pref("network.http.max-connections-per-server", 32);
 pref("network.http.pipelining", true);
 pref("network.http.pipelining.maxrequests", 8);
 pref("network.http.proxy.pipelining", true);
-
+pref("dom.ipc.plugins.enabled.libflashplayer.so", false);
+pref("dom.ipc.plugins.enabled.libnptest.so", false);
